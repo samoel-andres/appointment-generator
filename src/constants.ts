5ff14215612cd610d58bfaa8c0faa90aed777187
sav <aevs.andres.vidal@gmail.com>
2025-02-13 02:26:14 -0600

    feat: add file of global constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+
+const CONSTANTS = {
+    OUTPUT_FILE: path.resolve(__dirname, '../files/output/generated_appointments.xlsx'),
+    INPUT_FILE: path.resolve(__dirname, '../files/input/folios.txt')
+}
+
+export { CONSTANTS }
